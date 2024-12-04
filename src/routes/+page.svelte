7cf5608b75fbcf72b2737cbc899dd24877855904
Sirallens <ing.jorge.eduardo.le@gmail.com>
2024-12-03 21:07:05 -0600

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+
+<div class="container">
+    <h1 class="fully-centered"> Bienvenido </h1>
+</div>
+
+<style >
+
+    .container{
+
+        display:flex;
+        min-height: 100vh;
+        justify-content: center;
+        align-items: center;
+
+    }
+
+    .fully-centered{
+        font-size: 10rem;
+    }
+
+</style>
